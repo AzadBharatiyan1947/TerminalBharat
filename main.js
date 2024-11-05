8743befa8354af2532a7e08ae5373e11e9cf8d46
AzadBharatiyan1947 <pubgmobilekorean77@gmail.com>
2024-11-05 17:00:37 +0530

    UPD 57

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert("!!! MAINTAINENCE MODE !!!")
+alert("Disclaimer : This Website May Collect Cookies And Usage Report In Order To Provide & Ensure Secure Enviorment,It May Also Used To Enhance The User's Experience.Kindly Verify The T&C And Code Of Conduct.Thank You")
